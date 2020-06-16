bed84db48bd7964db655be5a44b45e7336967cbe
Pablo Giraldo <pablogirald@yahoo.es>
2020-06-16 09:20:59 +0200

    Add base files

---
+++ src/main/java/info/pablogiraldo/nghello/NghelloApplication.java
@@ -0,0 +1,13 @@
+package info.pablogiraldo.nghello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NghelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NghelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/pablogiraldo/nghello/NghelloApplicationTests.java
@@ -0,0 +1,13 @@
+package info.pablogiraldo.nghello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NghelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
